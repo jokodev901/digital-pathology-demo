81f2839bada9a3c07a4c59e9c79d903961e66001
jokodev901 <jokodev901@gmail.com>
2025-12-22 19:47:36 -0600

    Added ssl support and cert/key filepath parameters for nginx, added .env.template file

--- digital_pathology_demo/settings.py
+++ digital_pathology_demo/settings.py
@@ -30,6 +30,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+SECUREPROXYSSLHEADER = ("HTTPXFORWARDEDPROTO", "https")
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS","127.0.0.1").split(",")
 CSRF_TRUSTED_ORIGINS = os.environ.get("DJANGO_TRUSTED_ORIGINS","http://127.0.0.1").split(",")
 
