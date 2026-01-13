20bcc85d45d7e0ec3741c3920ef399bcbd735a8c
jokodev901 <jokodev901@gmail.com>
2026-01-12 20:41:45 -0600

    bugfix: added HTTP_X_FORWARDED_PROTO to testcase client to properly handle test requests without nginx proxy, re-organized some settings.py and nginx.conf entries

--- digital_pathology_demo/settings.py
+++ digital_pathology_demo/settings.py
@@ -32,12 +32,13 @@ DEBUG = os.environ.get('DJANGO_DEBUG', False) == 'True'     # Setting bool in .e
 
 # SSL, session, cookie, and csrf security settings
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
-SECURE_HSTS_SECONDS = 31536000
 SECURE_SSL_REDIRECT = True
+SECURE_HSTS_SECONDS = 31536000
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
-SECURE_HSTS_PRELOAD = True
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS","127.0.0.1").split(",")
 CSRF_TRUSTED_ORIGINS = os.environ.get("DJANGO_TRUSTED_ORIGINS","http://127.0.0.1").split(",")
